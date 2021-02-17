644cb0ca016f3a32c7900350dd7de4cd18a8d876
GCanoSandoval <gcanosandoval@csumb.edu>
2021-02-16 19:31:53 -0800

    hello

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by user190934 on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+}
